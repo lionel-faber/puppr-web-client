6f4501571c477c73369b6f61c49256729ea9a6af
Lionel Faber <lionel1704@gmail.com>
2020-09-26 01:39:46 +0530

    integrate with the backend API

--- components/addpup.js
+++ components/addpup.js
@@ -1,5 +1,4 @@
-import { Form, Input, InputNumber, Upload, Button, Select } from 'antd';
-import { UploadOutlined } from '@ant-design/icons'
+import { Form, Input, InputNumber, Button, Select } from 'antd';
 import styles from "./AddPup.module.css"
 
 const layout = {
@@ -22,31 +21,32 @@ export default function AddPup() {
 
     const [form] = Form.useForm();
 
-    const onFinish = pet => {
-        // var options = {
-        //     'method': 'POST',
-        //     'url': 'puppr-india.herokuapp.com/api/pets/add',
-        //     'headers': {
-        //     },
-        //     formData: {
-        //         'petPhoto': {
-        //             // 'value': fs.createReadStream(pet.photo),
-        //             // 'options': {
-        //             //     'filename': '4.jpeg',
-        //             //     'contentType': null
-        //             // }
-        //         },
-        //         'name': pet.name,
-        //         'gender': pet.gender,
-        //         'age': pet.age,
-        //         'about': pet.about
-        //     }
-        // };
-        // request(options, function (error, response) {
-        //     if (error) throw new Error(error);
-        //     console.log(response.body);
-        // });
-        console.log(pet);
+    const onFinish = async (pet) => {
+        var formData = new FormData();
+        formData.append('name', pet.name);
+        formData.append('gender', pet.gender);
+        formData.append('age', pet.age);
+        formData.append('about', pet.about);
+        var photo = document.querySelector('input[type="file"]')
+        formData.append('petPhoto', photo.files[0]);
+        var requestOptions = {
+            method: 'POST',
+            body: formData,
+            redirect: 'follow'
+          };
+          
+        try {
+            const response = await fetch("http://localhost:5000/api/pets/add", requestOptions);
+            if (response.status == 200) {
+                form.resetFields();
+                alert("Pet added!")
+            } else {
+                alert("Unexpected response: ", response);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+        
     };
 
     const onReset = () => {
@@ -55,32 +55,27 @@ export default function AddPup() {
 
     return (
         <Form form={form} {...layout} onFinish={onFinish}>
-            <Form.Item name={['name']} label="Name" rules={[{ required: true, }]}>
+            <Form.Item name="name" label="Name" rules={[{ required: true, message: "Please enter a name" }]}>
                 <Input />
             </Form.Item>
-            <Form.Item name={['gender']} label="Gender" rules={[{ required: true }]}>
+            <Form.Item name="gender" label="Gender" rules={[{ required: true, message: "Please choose a gender" }]}>
                 <Select>
                     <Select.Option value="male">male</Select.Option>
                     <Select.Option value="female">female</Select.Option>
                 </Select>
             </Form.Item>
-            <Form.Item name={['age']} label="Age" rules={[{ type: 'number', required: true, min: 0, max: 99 }]}>
+            <Form.Item name="age" label="Age" rules={[{ type: 'number', required: true, min: 0, max: 99, message: "Please enter the age" }]}>
                 <InputNumber />
             </Form.Item>
-            <Form.Item name={['introduction']} label="Introduction" rules={[{ required: true }]}>
+            <Form.Item name="about" label="Introduction" rules={[{ required: true, message: "A few words would be nice" }]}>
                 <Input.TextArea />
             </Form.Item>
             <Form.Item
-                name={['photo']}
+                name="petPhoto"
                 label="Photo"
-                valuePropName="photo"
-                rules={[{ required: true }]}
+                rules={[{ required: true, message: "A picture says a thousand words" }]}
             >
-                <Upload name="logo" listType="picture">
-                    <Button>
-                        <UploadOutlined /> Click to upload
-                    </Button>
-                </Upload>
+                <Input type="file" accept="image/*" capture="camera"/>
             </Form.Item>
             <Form.Item {...tailLayout}>
                 <Button type="primary" htmlType="submit" className={styles.formButton}>
